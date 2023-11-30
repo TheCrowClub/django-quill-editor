f30c0f7283e559e80abd08f2e2fad13f4b0c32e6
Daniel <61970262+rozari0@users.noreply.github.com>
2023-11-30 14:10:18 +0600

    Update django_quill.js

--- django_quill/static/django_quill/django_quill.js
+++ django_quill/static/django_quill/django_quill.js
@@ -16,5 +16,44 @@ class QuillWrapper {
             var data = {delta: delta, html: html};
             this.targetInput.value = JSON.stringify(data);
         });
+        this.quill.getModule('toolbar').addHandler('image', () => {
+            selectLocalImage();
+          });
     }
 }
+
+
+function selectLocalImage() {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('multiple', 'multiple');
+    input.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/webp')
+    input.click();
+  
+    // Listen upload local image and save to server
+    input.onchange = () => {
+      const fileList = Array.from(input.files);
+      saveToServer(fileList);
+    }
+  }
+    
+  function saveToServer(files) {
+    const formData = new FormData();
+    files.forEach(file => formData.append('images[]', file));
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '//' + window.location.host + '/upload/image', true);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText).data;
+        data.forEach(url => insertToEditor(url));
+      }
+    };
+    xhr.send(formData);
+  }
+  
+  function insertToEditor(url) {
+    // push image url to rich editor.
+    const range = quill.getSelection();
+    quill.insertEmbed(range.index, 'image', url);
+  }  
